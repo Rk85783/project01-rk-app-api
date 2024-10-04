01ea5406d8d8466b13a0fc5549f255daa49401c7
Rohit Kumar Mahor <rk85783@gmail.com>
2024-10-04 15:43:52 +0530

    fix delete admin api issue

--- routes/api.route.js
+++ routes/api.route.js
@@ -15,7 +15,7 @@ router.post("/admin/create", [authorizedToken, hasRole(["SUPER_ADMIN"])], adminC
 router.get("/admin/list", [authorizedToken, hasRole(["SUPER_ADMIN"])], adminList);
 router.get("/admin/view/:id", [authorizedToken, hasRole(["SUPER_ADMIN"])], adminView);
 router.put("/admin/edit/:id", [authorizedToken, hasRole(["SUPER_ADMIN"])], adminEdit);
-router.delete("/admin/get/:id", [authorizedToken, hasRole(["SUPER_ADMIN"])], adminDelete);
+router.delete("/admin/delete/:id", [authorizedToken, hasRole(["SUPER_ADMIN"])], adminDelete);
 router.get("/admin/profile", [authorizedToken, hasRole(["SUPER_ADMIN", "ADMIN"])], adminProfile);
 
 // ----------------> User Controller Routes
